04950e93b75865d0d33c2b914810fc7e5be1595e
Awadyking <eali83051@gmail.com>
2025-06-18 01:34:40 +0300

    Finish Home Page

--- src/routes/E404.tsx
+++ src/routes/E404.tsx
@@ -1,15 +1,16 @@
 import type { TFunction } from "i18next";
 import { useTranslation } from "react-i18next"; 
+
 export default function E404() {
 
-  const {t } : {t : TFunction<"translation" , undefined>} = useTranslation()
-
+  const {t} : {t : TFunction<"translation" , undefined>} = useTranslation()
 
     return (
         <>
-        <img src="/img/404.png" className="w-60 h-fit "/>
+        <img src="/img/404.png" className="w-56 h-fit "/>
         <p className="text-3xl font-bold text-black dark:text-white mt-2">404 | {t("not_found")}</p>
-        <p className="text-2xl font-bold text-black text-center dark:text-white mt-2 w-6/12 mt-1">{t("not_found_msg")}</p>
+        <p className="text-2xl font-bold text-black text-center dark:text-white mt-3 lg:w-6/12 w-10/12 ">{t("not_found_msg")}</p>
+
         </>
     )
 }
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,13 +1,38 @@
 import type { TFunction } from "i18next";
 import { useTranslation } from "react-i18next"; 
 import i18n from "../i18next"; 
+import type { ValueType } from "../redux/reducers/Main_reducer";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
 export default function Home() {
   const {t , i18n} : {t : TFunction<"translation" , undefined> , i18n : any} = useTranslation()
+  const {token} : {token : string | undefined} = useSelector((state : ValueType) => state)
 
+  
 
   return (
     <>
-    {t("f")}
+    <img src="/img/intro.png" className="w-56 h-fit "/>
+    <p className="text-xl lg:text-2xl font-bold text-black dark:text-white mt-2">{t("welcome")}</p>
+    <p className="text-xl lg:text-2xl font-bold text-black dark:text-white mt-2 text-center ">{t("welcome_msg")}</p>
+    {
+      token == "" ? <div className="flex w-7/12 lg:w-3/12 mt-4 justify-between">
+        <Link to="/login">
+      <button className="btn bg-[#FF9393] border-[#FF9393] rounded-4xl w-28">
+        <img src="/img/login_btn.svg" className="w-6 h-6"/>
+        {t("login_btn")}</button>
+        </Link>
+       <Link to="/register"> 
+  <button className="btn btn-outline btn-error rounded-4xl w-28">
+     {t("register_btn")}
+     </button>
+     </Link>
+
+      </div> : <Link to="/dashboard">
+      <button className="btn btn-error rounded-4xl w-28 mt-4">{t("dashboard_btn")}</button>
+      </Link>
+    }
     </>
   )
 }
